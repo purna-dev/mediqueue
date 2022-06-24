740c5ac1863023d3b4235288f443a30ce7904302
Purna Chandra Saren <65004060+purna-dev@users.noreply.github.com>
2022-06-24 18:47:31 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".mySwiper", {
+        speed: 600,
+        parallax: true,
+		autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+
+
+
+
+
+      function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
